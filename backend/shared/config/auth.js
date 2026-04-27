67798a74489b1e8448857438a054fdab925a7a03
MiraPhilip12 <miraphilip2012@gmail.com>
2026-04-28 02:52:36 +0300

    Rebooting project

--- backend/shared/config/auth.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    jwtSecret: process.env.JWT_SECRET || '0774a2cb1d1c5cc1569e114091482b019e8f922c947d86b2be864404310a3903',
-    jwtExpire: process.env.JWT_EXPIRE || '7d',
-    bcryptRounds: parseInt(process.env.BCRYPT_ROUNDS) || 10
-};
